aeb1524a8228ea27642ef76666b2f93610341dc9
olafire <olafire03@gmail.com>
2022-04-06 21:57:08 +0100

    database change

--- Startup.cs
+++ Startup.cs
@@ -27,9 +27,11 @@ namespace CyberTravels
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
+           services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(
                     Configuration.GetConnectionString("DefaultConnection")));
+
+
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
